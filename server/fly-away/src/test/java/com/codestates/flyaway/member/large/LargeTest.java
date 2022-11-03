f49218666745df9b25db364a53c71d2afef3cd0a
sungmingt <qotjdals8313@naver.com>
2022-11-04 00:34:07 +0900

    TEST : 테스트 코드 수정 및 추가

---
+++ server/fly-away/src/test/java/com/codestates/flyaway/member/large/LargeTest.java
@@ -0,0 +1,136 @@
+package com.codestates.flyaway.member.large;
+
+import com.auth0.jwt.JWT;
+import com.codestates.flyaway.domain.login.service.LoginService;
+import com.codestates.flyaway.domain.member.entity.Member;
+import com.codestates.flyaway.domain.member.repository.MemberRepository;
+import com.codestates.flyaway.domain.member.service.MemberService;
+import com.codestates.flyaway.domain.record.entity.Record;
+import com.codestates.flyaway.domain.record.repository.RecordRepository;
+import com.codestates.flyaway.domain.record.service.RecordService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import static com.codestates.flyaway.domain.member.util.MemberUtil.*;
+import static com.codestates.flyaway.web.login.dto.LoginDto.*;
+import static com.codestates.flyaway.web.member.dto.MemberDto.*;
+import static com.codestates.flyaway.web.record.dto.RecordDto.*;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.DynamicTest.*;
+
+@SpringBootTest
+class LargeTest {
+
+    @Autowired
+    private MemberService memberService;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private LoginService loginService;
+
+    @Autowired
+    private RecordService recordService;
+
+    @Autowired
+    private RecordRepository recordRepository;
+
+    @DisplayName("member/record large test")
+    @TestFactory
+    Stream<DynamicTest> joinAndLogin() {
+        final String name = "jason";
+        final String email = "jason12@gmail.com";
+        final String password = "asdf1234!";
+
+        return Stream.of(
+                dynamicTest("회원 가입 진행", () -> {
+                    //given
+                    JoinRequest req = new JoinRequest(name, email, password);
+
+                    //when
+                    JoinResponse res = memberService.join(req);
+
+                    //then
+                    assertThat(res.getEmail()).isEqualTo(req.getEmail());
+                    assertThat(res.getName()).isEqualTo(req.getName());
+                }),
+
+                dynamicTest("로그인 시도", () -> {
+                    //given
+                    LoginRequest req = new LoginRequest(email, password);
+
+                    //when
+                    String token = loginService.login(req);
+                    String accessToken = token.replace("Bearer ", "");
+
+                    //then
+                    assertThat(JWT.decode(accessToken)
+                            .getClaim("email").asString())
+                            .isEqualTo(email);
+                }),
+
+                dynamicTest("운동하기", () -> {
+                    //given
+                    Long memberId = 1L;
+                    Long record = 1000L;
+                    InsertRequest request = new InsertRequest(record);
+
+                    //when
+                    InsertResponse response = recordService.insertRecord(memberId, request);
+
+                    //then
+                    List<Record> records = recordRepository.findByMemberId(memberId);
+
+                    assertThat(records).hasSize(1);
+                    assertThat(records.get(0).getRecord()).isEqualTo(record);
+                }),
+
+                dynamicTest("프로필 조회", () -> {
+                    //given
+                    Long memberId = 1L;
+
+                    //when
+                    MemberProfileResponse response = memberService.findByIdFetch(memberId);
+
+                    //then
+                    assertThat(response.getEmail()).isEqualTo(email);
+                    assertThat(response.getName()).isEqualTo(name);
+                    assertThat(response.getTotalRecord()).isEqualTo(1000L);
+                }),
+
+                dynamicTest("회원 정보 수정", () -> {
+                    //given
+                    UpdateRequest req = new UpdateRequest(1L, "jordan", "newpass123!", null);
+
+                    //when
+                    memberService.update(req);
+
+                    //then    변경감지가 db에 반영이 안된 것 같은데 테스트에 성공했다... 뭐지??
+                    Member member = memberRepository.findById(1L).get();
+
+                    assertThat(member.getName()).isEqualTo(req.getName());
+                    assertThat(member.getPassword()).isEqualTo(encode(req.getPassword()));
+                }),
+
+                dynamicTest("회원 탈퇴", () -> {
+                    //given
+                    long memberId = 1L;
+
+                    //when
+                    memberService.delete(memberId);
+
+                    //then
+                    Optional<Member> member = memberRepository.findByEmail(email);
+                    assertThat(member).isEmpty();
+                })
+        );
+    }
+}
--- server/fly-away/src/test/java/com/codestates/flyaway/record/repository/RecordRepositoryTest.java
+++ server/fly-away/src/test/java/com/codestates/flyaway/record/repository/RecordRepositoryTest.java
@@ -1,47 +1,43 @@
-//package com.codestates.flyaway.record.repository;
-//
-//import com.codestates.flyaway.domain.member.entity.Member;
-//import com.codestates.flyaway.domain.member.repository.MemberRepository;
-//import com.codestates.flyaway.domain.record.entity.Record;
-//import com.codestates.flyaway.domain.record.repository.RecordRepository;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-//
-//import java.util.ArrayList;
-//
-//import static java.time.LocalDate.now;
-//import static org.assertj.core.api.Assertions.assertThat;
-//
-//@DataJpaTest
-//class RecordRepositoryTest {
-//
-//    @Autowired
-//    private MemberRepository memberRepository;
-//    @Autowired
-//    private RecordRepository recordRepository;
-//
-//    @BeforeEach
-//    void before() {
-//        Member member1 = new Member(1L, null, new ArrayList<>(), "kim", "member1@gmail.com", "pw");
-//
-//        Record record1 = new Record(now(), 10);
-//        record1.setMember(member1);
-//
-//        memberRepository.save(member1);
-//        recordRepository.save(record1);
-//    }
-//
-//
-//    @DisplayName("회원 ID와 날짜를 기반으로 기록 조회 (findByMemberIdAndDate)")
-//    @Test
-//    void findRecordTest() {
-//
-//        Record record = recordRepository.findByMemberIdAndDate(1L, now()).get();
-//
-//        assertThat(record.getMember().getId()).isEqualTo(1);
-//        assertThat(record.getRecord()).isEqualTo(10);
-//    }
-//}
+package com.codestates.flyaway.record.repository;
+
+import com.codestates.flyaway.domain.member.entity.Member;
+import com.codestates.flyaway.domain.member.repository.MemberRepository;
+import com.codestates.flyaway.domain.record.entity.Record;
+import com.codestates.flyaway.domain.record.repository.RecordRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static java.time.LocalDate.now;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+class RecordRepositoryTest {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private RecordRepository recordRepository;
+
+    @DisplayName("회원 ID와 날짜로 운동 기록 조회 (findByMemberIdAndDate)")
+    @Test
+    void findRecordTest() {
+        //given
+        Member member = new Member("김코딩", "kimcode@gmail.com", "password123@");
+        Record record = new Record(now(), 10);
+        record.setMember(member);
+
+        memberRepository.save(member);
+        recordRepository.save(record);
+
+        //when
+        Record findRecord = recordRepository.findByMemberIdAndDate(member.getId(), now())
+                .orElseThrow(() -> new RuntimeException("###"));
+
+        //then
+        assertThat(findRecord.getMember().getId()).isEqualTo(member.getId());
+        assertThat(findRecord.getRecord()).isEqualTo(10);
+    }
+}
