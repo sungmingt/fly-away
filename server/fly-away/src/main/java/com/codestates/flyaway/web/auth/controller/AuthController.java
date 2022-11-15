cb0e4d4a0ed3afd7649ecab2e2d1d8e325ebbcfa
sungmingt <qotjdals8313@naver.com>
2022-11-15 21:29:53 +0900

    REFACTOR : 코드 수정 (static 변수 활용)

--- server/fly-away/src/main/java/com/codestates/flyaway/web/auth/controller/AuthController.java
+++ server/fly-away/src/main/java/com/codestates/flyaway/web/auth/controller/AuthController.java
@@ -11,8 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import static com.codestates.flyaway.domain.auth.util.JwtUtil.AUTHORIZATION;
-import static com.codestates.flyaway.domain.auth.util.JwtUtil.PREFIX;
+import static com.codestates.flyaway.domain.auth.util.JwtUtil.*;
 import static com.codestates.flyaway.web.auth.dto.LoginDto.*;
 
 @RestController
@@ -27,16 +26,16 @@ public class AuthController {
     public String login(@RequestBody LoginRequest loginRequest, HttpServletResponse response) {
         LoginResponse loginInfo = authService.login(loginRequest);
 
-        response.addHeader("memberId", String.valueOf(loginInfo.getMemberId()));
+        response.addHeader("memberId", loginInfo.getMemberId());
         response.addHeader(AUTHORIZATION, loginInfo.getAccessToken());
 
-        return ("로그인 성공");
+        return "로그인 성공";
     }
 
     @ApiOperation(value = "로그아웃 API")
     @PostMapping("/logout")
     public String logout(HttpServletRequest request) {
-        String email = (String) request.getAttribute("email");
+        String email = (String) request.getAttribute(EMAIL);
         String accessToken = request.getHeader(AUTHORIZATION).replace(PREFIX, "");
         log.info("### 로그아웃 요청 - {}", email);
 
